6b3bf05c1caa1b7b3a03395911ddd6b3e51b8af1
Edwin DeNicholas <disruption999@hotmail.com>
2021-10-02 12:04:40 -0700

    Add level one tones, random stable duration, main mixer.

--- Assets/Scripts/UnstableTone.cs
+++ Assets/Scripts/UnstableTone.cs
@@ -11,11 +11,13 @@ public class UnstableTone : MonoBehaviour
 
     [Range(0.1f, 10f)]
     public float driftDuration = 1;
+    public float driftScale = 1;
     public AnimationCurve drift;
 
     [Range(1f, 10f)]
     public float stableDuration = 5;
 
+    private float currentStableDuration = 0;
     private float driftElapsedTime;
     private float stabilityElapsedTime;
 
@@ -27,10 +29,10 @@ public class UnstableTone : MonoBehaviour
     private void Update()
     {
         stabilityElapsedTime += Time.deltaTime;
-        if (stabilityElapsedTime > stableDuration)
+        if (stabilityElapsedTime > currentStableDuration)
         {
             driftElapsedTime = (driftElapsedTime + Time.deltaTime) % driftDuration;
-            audioSource.pitch = initialPitch + drift.Evaluate(driftElapsedTime / driftDuration);
+            audioSource.pitch = initialPitch + drift.Evaluate(driftElapsedTime / driftDuration) * driftScale;
         }
     }
 
@@ -39,5 +41,6 @@ public class UnstableTone : MonoBehaviour
         audioSource.pitch = initialPitch;
         stabilityElapsedTime = 0;
         driftElapsedTime = 0;
+        currentStableDuration = stableDuration + Random.Range(-(stableDuration / 2), stableDuration / 2);
     }
 }
